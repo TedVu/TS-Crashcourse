6b1ce27aa3732fd82b00f92440d2682a6357b4b2
TedEngineer <tedvu184@gmail.com>
2021-11-16 22:04:54 +1100

    Adding some basic code to learn about types

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
+// We can assign types
 let id: number = 5;
+
+let company: string = "Ted";
+
+let isPublished: boolean = true;
+
+// will not be complaint
+let x: any = "Hello";
+
+x = true;
+
+// array of numbers
+let ids: number[] = [1, 2, 3, 4, 5];
+
+// tuple type
+let person: [number, string, boolean] = [1, "ted", true];
+
+// tuple array
+let employee: [number, string, boolean][] = [
+  [1, "Ted", true],
+  [2, "Kevin", true],
+];
+
+// union
+let uid: string | number;
+uid = "ted";
+uid = 5;
+
+// Enum - allow us to declare a set of named constant
